9cebdf05bea623ba6e05926bb66e340f92b9be0c
ERIKQQY6 <84676290+ERIKQQY6@users.noreply.github.com>
2021-06-21 22:39:49 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ module.exports = {
   projectName: 'Casnode-website', // Usually your repo name.
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh', 'fr', 'de', 'ru', 'jp']
+    locales: ['en', 'zh', 'fr', 'de', 'ru', 'jp', 'ko']
   },
   themeConfig: {
     navbar: {
