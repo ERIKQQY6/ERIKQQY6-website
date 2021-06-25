a6be23109cd3a0584b715060f6bbca1a3dd95051
ERIKQQY6 <84676290+ERIKQQY6@users.noreply.github.com>
2021-06-25 15:32:03 +0800

    Update baiduAnalytics.js

--- plugins/baiduAnalytics.js
+++ plugins/baiduAnalytics.js
@@ -1,8 +1,8 @@
 module.exports = function baiduAnalytics(context, options) {
-    var _hmt = _hmt || [];
     return {
         name: 'baiduAnalytics',
         async loadContent(){
+            var _hmt = _hmt || [];
             var hm = document.createElement("script");
             hm.src = "https://hm.baidu.com/hm.js?<my code>";
             var s = document.getElementsByTagName("script")[0];
